1117d95f34f35107fb697ae9399545a3ec6fbed9
venugopal <venuputlur@gmail.com>
2018-06-17 17:22:29 +0530

    reddy

---
+++ Desktop/selenium workspace/SAMPLE/src/main/java/SAMPLE/SAMPLE/App.java
@@ -0,0 +1,13 @@
+package SAMPLE.SAMPLE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
